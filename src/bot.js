128c16aecc6a413cb8715ae46e2094aa9d70ba32
BuildTools <rtreertree@gmail.com>
2022-01-13 21:06:22 +0700

    add prefix and func

--- src/bot.js
+++ src/bot.js
@@ -2,11 +2,31 @@ require('dotenv').config();
 
 console.log(`Login with token ${process.env.BOT_TOKEN}`);
 
-const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const {
+    Client,
+    Intents
+} = require('discord.js');
 
-client.on('ready', ()=>{
-    console.log(`${client.user.tag} was login`);
+const client = new Client({
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
+});
+
+const PREFIX = '!';
+
+
+
+client.on('ready', () => {
+    console.log(`Bot ${client.user.tag} is logged in!`);
+});
+
+client.on('messageCreate', (message)=>{
+    if (message.author.bot === true) return;
+    if(message.content.startsWith(PREFIX)){
+        const [cmdName, ...args] = message.content.trim().substring(PREFIX.length).split(/\s+/);
+
+        message.reply(cmdName);
+        console.log(args);
+    }
 });
 
 
