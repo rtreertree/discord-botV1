14fc2765a72dc12999f66fb17bc4a6a15600f643
BuildTools <rtreertree@gmail.com>
2022-01-14 11:04:20 +0700

    create kick command

--- src/bot.js
+++ src/bot.js
@@ -13,6 +13,10 @@ const client = new Client({
 
 const PREFIX = '!';
 
+//function zone//
+
+
+/////////////////
 
 
 client.on('ready', () => {
@@ -21,12 +25,28 @@ client.on('ready', () => {
 
 client.on('messageCreate', (message)=>{
     if (message.author.bot === true) return;
-    if(message.content.startsWith(PREFIX)){
-        const [cmdName, ...args] = message.content.trim().substring(PREFIX.length).split(/\s+/);
+    if(message.content.startsWith(PREFIX) === false) return;
+    const [cmdName, ...args] = message.content.trim().substring(PREFIX.length).split(/\s+/);
 
-        message.reply(cmdName);
-        console.log(args);
+    switch(cmdName) {
+        case 'random':
+            if(args.length === 0){
+                message.reply((Math.floor(Math.random() * 100)).toString());
+                console.log(Math.random() * 100);
+            }else if(args.length === 1){
+                message.reply((Math.floor(Math.random() * args[0])).toString());
+            }
+            break;
+        case 'kick':
+            if(args.length !== 1) return message.reply('Use !kick <Member>');
+            const member = message.mentions.members.first();
+            if (member.kickable === true){
+                member.kick('You kick by someone');
+            } else {
+                message.reply('this bot it not enough permissions to kick this member');
+            }
     }
+
 });
 
 
